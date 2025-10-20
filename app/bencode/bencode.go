66fd18b7f9d9bbb0723f1646756c39278c841def
sahil-singh1107 <sahilpython@gmail.com>
2025-10-20 18:35:04 +0530

    decode dictionaries

--- app/bencode/bencode.go
+++ app/bencode/bencode.go
@@ -20,6 +20,8 @@ func (d *Decoder) getType(input string) string {
 		return "integer"
 	} else if input[0] == 'l' {
 		return "list"
+	} else if input[0] == 'd' {
+		return "dictionary"
 	}
 	return ""
 }
@@ -32,6 +34,8 @@ func (d *Decoder) Decode(input string) (any, error) {
 		res = d.decodeInteger(input)
 	} else if d.getType(input) == "list" {
 		res = d.decodeList(input)
+	} else if d.getType(input) == "dictionary" {
+		res = d.decodeDictionary(input)
 	} else {
 		return "", fmt.Errorf("Only strings and integers are supported at the moment")
 	}
@@ -110,4 +114,84 @@ func (d *Decoder) decodeList(input string) []any {
 	}
 
 	return result
+}
+
+func (d *Decoder) decodeDictionary(input string) map[string]any {
+	res := make(map[string]any)
+	i := 1
+
+	for i < len(input) && input[i] != 'e' {
+		colonIndex := i
+		for ; colonIndex < len(input); colonIndex++ {
+			if input[colonIndex] == ':' {
+				break
+			}
+		}
+		length, _ := strconv.Atoi(input[i:colonIndex])
+		start := colonIndex + 1
+		end := start + length
+		key := input[start:end]
+		i = end
+		switch input[i] {
+		case 'i':
+			endIndex := i + 1
+			for ; endIndex < len(input); endIndex++ {
+				if input[endIndex] == 'e' {
+					break
+				}
+			}
+			num, _ := strconv.Atoi(input[i+1 : endIndex])
+			res[key] = num
+			i = endIndex + 1
+
+		case 'l':
+			startIndex := i
+			depth := 1
+			j := i + 1
+			for ; j < len(input); j++ {
+				if input[j] == 'l' {
+					depth++
+				} else if input[j] == 'e' {
+					depth--
+					if depth == 0 {
+						break
+					}
+				}
+			}
+			res[key] = d.decodeList(input[startIndex : j+1])
+			i = j + 1
+
+		case 'd':
+			startIndex := i
+			depth := 1
+			j := i + 1
+			for ; j < len(input); j++ {
+				if input[j] == 'd' {
+					depth++
+				} else if input[j] == 'e' {
+					depth--
+					if depth == 0 {
+						break
+					}
+				}
+			}
+			res[key] = d.decodeDictionary(input[startIndex : j+1])
+			i = j + 1
+
+		default:
+			colonIndex := i
+			for ; colonIndex < len(input); colonIndex++ {
+				if input[colonIndex] == ':' {
+					break
+				}
+			}
+			length, _ := strconv.Atoi(input[i:colonIndex])
+			start := colonIndex + 1
+			end := start + length
+			res[key] = input[start:end]
+			i = end
+		}
+	}
+
+	return res
 }
